6278d5656be4ea4961e21eb8224eb2f0338121d2
Chris Chuang <chrischuang0421@gmail.com>
2020-01-20 17:31:03 +0800

    Update 3rd-party packages.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'normalize.css';
-import './index.build.css';
-import App from './js/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "normalize.css";
+import "./index.build.css";
+import App from "./js/App";
 //import registerServiceWorker from './js/registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 //registerServiceWorker();
